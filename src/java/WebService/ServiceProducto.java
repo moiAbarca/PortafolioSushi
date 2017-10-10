2fe1c41fb5fe969dc69b032b4f1a3af2ee9601a4
moi <moi@10.211.55.3>
2017-10-10 12:36:35 -0300

    Modificado WebService Producto listo agregar

--- src/java/WebService/ServiceProducto.java
+++ src/java/WebService/ServiceProducto.java
@@ -20,8 +20,8 @@ import javax.jws.WebParam;
 @WebService(serviceName = "ServiceProducto")
 public class ServiceProducto {
 
-ProductoDao dao = new OracleProducto();
-Producto producto = new Producto();    
+
+
     /**
      * This is a sample web service operation
      */
@@ -32,23 +32,14 @@ Producto producto = new Producto();
     
     @WebMethod(operationName = "agregaProducto")        
     public void agregaProducto(
-                                @WebParam(name = "imagenProducto") String imagenProducto,
-                                @WebParam(name = "nombreProducto") String nombreProducto,
-                                @WebParam(name = "descripcionProducto") String descripcionProducto,
-                                @WebParam(name = "categoriaProducto") int categoriaProducto,
-                                @WebParam(name = "disponibilidaProducto") boolean disponibilidaProducto,
-                                @WebParam(name = "porcionesProdcuto") int porcionesProdcuto,
-                                @WebParam(name = "precioProducto") int precioProducto,
-                                @WebParam(name = "productoId") int productoId
-                               ) throws SQLException {
-        producto.setImagenProdcuto(imagenProducto);
-        producto.setNombreProducto(nombreProducto);
-        producto.setDescripcionProducto(descripcionProducto);
-        producto.setCategoriaProductoId(categoriaProducto);
-        producto.setDisponibilidadProducto(disponibilidaProducto);
-        producto.setPorcionesProdcuto(porcionesProdcuto);
-        producto.setPrecioProducto(precioProducto);
-        producto.setProductoId(productoId);
+                                @WebParam(name = "producto") Producto producto                             
+                               )  
+    {
+        try{
+        ProductoDao dao = new OracleProducto();
         dao.agregarProducto(producto);
+        }
+        catch(Exception ex)
+        {}
     }
 }
