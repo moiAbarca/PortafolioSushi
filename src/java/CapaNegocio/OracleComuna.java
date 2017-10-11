578b5507bb898c454c011f48b405d14db45af593
moi <moi@10.211.55.3>
2017-10-11 14:35:22 -0300

    9 clases de negocio listas

---
+++ src/java/CapaNegocio/OracleComuna.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CapaNegocio;
+
+import CapaConexion.Conecta;
+import CapaDTO.Comuna;
+import Clasesinterface.ComunaDao;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import oracle.jdbc.OracleTypes;
+
+/**
+ *
+ * @author moi
+ */
+public class OracleComuna implements ComunaDao{
+     //llama a la clase conecta...para conectarse a la BD
+    public Conecta db;    
+    public OracleComuna()
+    {
+        //constructor vacío
+        db = new Conecta();
+    }
+    
+    //Inicio de implementación de métodos de la Interface
+
+    @Override
+    public List<Comuna> obtenerComuna() throws SQLException {
+        //Crea una lista de tipo Acceso
+        List<Comuna> cComuna = new ArrayList<Comuna>();  
+        //instancia la clase acceso
+        Comuna bComuna = new Comuna();
+        String sql = null;
+        Connection con = null;  
+        //CallableStatement = me permite ejecutar sentencias SQL
+        CallableStatement cs = null;
+        //ResultSet = proporciona varios métodos para obtener los datos de columna correspondientes a un fila
+        ResultSet rs;
+        try
+        {
+            //se conecta a la BD
+            con = db.getConnection();
+            //el SP tiene solo un parámetro de salida
+            //por eso solo tiene un ?
+            sql = "{call FUKUSUKESUSHI.LISTAR_COMUNA(?)}";
+            //le paso al CallableStatement la sentencia SQL
+            cs = con.prepareCall(sql);
+            cs.registerOutParameter(1, OracleTypes.CURSOR);
+            cs.execute();
+            //creo un resultset con todos los datos de la tabla Acceso
+            rs = (ResultSet)cs.getObject(1);
+            //el resulset por defecto comienza en un índice antes que mi resultado
+            //por lo que debo ponerlo en NEXT para que me devuelva desde el primer valor en adelante
+            //cuando se que v aa devolver varios valores ocupo while
+            //cuando se que va a devolver uno, ocupo un IF
+            while(rs.next())
+            {
+                bComuna = new Comuna();
+                bComuna.setComunaId(rs.getInt(1));
+                bComuna.setProvinciaId(rs.getInt(2));
+                bComuna.setComunaNombre(rs.getString(3));
+                cComuna.add(bComuna);
+            }
+            rs.close();
+            cs.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return cComuna;
+    }
+
+    @Override
+    public void agregarComuna(Comuna comuna) throws SQLException {
+        Comuna bComuna = new Comuna();
+        String sql = null;
+        Connection con = null;
+        CallableStatement cs = null;         
+        try
+        {            
+            con = db.getConnection();
+            //llama al insertar de la BD que tiene 3 parámetros de entrada 
+            sql = "{call FUKUSUKESUSHI.COMUNA_tapi.ins(?, ?, ?)}";
+            cs = con.prepareCall(sql);
+            //le seteo los 3 parámetros de entrada
+            cs.setString(1, comuna.getComunaNombre());
+            cs.setInt(2, comuna.getComunaId());
+            cs.setInt(3, comuna.getProvinciaId());
+            cs.execute();          
+            cs.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("No se pudo agregar: " + e);
+        }
+    }
+
+    @Override
+    public void modificarComuna(Comuna comuna) throws SQLException {
+        Comuna bComuna = new Comuna();
+        String sql = null;
+        Connection con = null;
+        CallableStatement cs = null;         
+        try
+        {            
+            con = db.getConnection();
+            //llama al update de la BD que tiene 3 parámetros de entrada 
+            sql = "{call FUKUSUKESUSHI.COMUNA_tapi.upd(?, ?, ?)}";
+            cs = con.prepareCall(sql);
+            cs.setString(1, comuna.getComunaNombre());
+            cs.setInt(2, comuna.getComunaId());
+            cs.setInt(3, comuna.getProvinciaId());
+            cs.execute();          
+            cs.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("No se pudo agregar: " + e);
+        }
+    }
+
+    @Override
+    public void eliminarComuna(Integer id) throws SQLException {
+        String sql = null;
+        Connection con = null;
+        CallableStatement cs = null;         
+        try
+        {            
+            con = db.getConnection();
+            //llama al eliminar que recibe 1 parámetro
+            sql = "{call FUKUSUKESUSHI.COMUNA_tapi.del(?)}";
+            cs = con.prepareCall(sql);
+            //entrego el parámetro de entrada de la función 
+            //y si existe en la BD lo borro
+            cs.setInt(1, id);
+            cs.execute();          
+            cs.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Comuna> buscarComuna(Integer id) throws SQLException {
+        List<Comuna> cComuna = new ArrayList<Comuna>();        
+        Comuna bComuna = new Comuna();
+        String sql = null;
+        Connection con = null;        
+        CallableStatement cs = null;
+        ResultSet rs;
+        try
+        {
+            con = db.getConnection();   
+            //llama a la función de tiene dos parámetros
+            //el primero de entrada
+            //y el segundo de salida
+            sql = ("{call FUKUSUKESUSHI.BUSCAR_COMUNA(?,?)}");            
+            cs = con.prepareCall(sql);
+            //seteo el primer parámetro
+            cs.setInt(1, id); 
+            //defino el tipo de dato que debe devolver
+            cs.registerOutParameter(2, OracleTypes.CURSOR);
+            cs.execute();
+            
+            //creo un resultset con todos los datos de la tabla Acceso
+            //devuelve el objeto OUT que se encuentra en el 2 parámetro
+            rs = (ResultSet)cs.getObject(2);
+            if(rs.next())
+            {
+                bComuna = new Comuna();
+                bComuna.setComunaId(rs.getInt(1));
+                bComuna.setProvinciaId(rs.getInt(2));
+                bComuna.setComunaNombre(rs.getString(3));
+                cComuna.add(bComuna);
+            }
+            rs.close();
+            
+            cs.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return cComuna;
+    }
+    
+}
