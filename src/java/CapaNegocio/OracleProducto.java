ee85d0be7fb39408c2473e2794d688a689653170
moi <moi@10.211.55.3>
2017-11-02 01:21:43 -0200

    Modificaado NegocioProducto, funcionan todos los webServices

--- src/java/CapaNegocio/OracleProducto.java
+++ src/java/CapaNegocio/OracleProducto.java
@@ -33,34 +33,21 @@ public class OracleProducto implements ProductoDao{
     //Inicio de implementación de métodos de la Interface
 
     @Override
-    public List<Producto> obtenerProducto() throws SQLException {
-        //Crea una lista de tipo Acceso
-        List<Producto> cProducto = new ArrayList<Producto>();  
-        //instancia la clase acceso
+    public List<Producto> obtenerProducto() throws SQLException {       
+        List<Producto> cProducto = new ArrayList<Producto>();          
         Producto bProducto = new Producto();
         String sql = null;
-        Connection con = null;  
-        //CallableStatement = me permite ejecutar sentencias SQL
-        CallableStatement cs = null;
-        //ResultSet = proporciona varios métodos para obtener los datos de columna correspondientes a un fila
+        Connection con = null;          
+        CallableStatement cs = null;        
         ResultSet rs;
         try
-        {
-            //se conecta a la BD
-            con = db.getConnection();
-            //el SP tiene solo un parámetro de salida
-            //por eso solo tiene un ?
-            sql = "{call FUKUSUKESUSHI.LISTAR_PRODUCTO(?)}";
-            //le paso al CallableStatement la sentencia SQL
+        {            
+            con = db.getConnection();         
+            sql = "{call FUKUSUKESUSHI.LISTAR_PRODUCTO(?)}";            
             cs = con.prepareCall(sql);
             cs.registerOutParameter(1, OracleTypes.CURSOR);
-            cs.execute();
-            //creo un resultset con todos los datos de la tabla Acceso
-            rs = (ResultSet)cs.getObject(1);
-            //el resulset por defecto comienza en un índice antes que mi resultado
-            //por lo que debo ponerlo en NEXT para que me devuelva desde el primer valor en adelante
-            //cuando se que v aa devolver varios valores ocupo while
-            //cuando se que va a devolver uno, ocupo un IF
+            cs.execute();            
+            rs = (ResultSet)cs.getObject(1);            
             while(rs.next())
             {
                 bProducto = new Producto();
@@ -71,7 +58,7 @@ public class OracleProducto implements ProductoDao{
                 bProducto.setPrecioProducto(rs.getInt(5));
                 bProducto.setDescripcionProducto(rs.getString(6));
                 bProducto.setImagenProdcuto(rs.getString(7));
-                bProducto.setDisponibilidadProducto(rs.getBoolean(7));
+                bProducto.setDisponibilidadProducto(rs.getBoolean(8));
                 
                 cProducto.add(bProducto);
             }
